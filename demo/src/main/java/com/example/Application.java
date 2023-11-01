c74c7b9a848d0fecad8bc13663aa14e58485add0
Your Name <you@example.com>
2023-11-01 16:20:35 +0700

    moving application

--- demo/src/main/java/com/example/Application.java
+++ demo/src/main/java/com/example/Application.java
@@ -1,3 +1,4 @@
+package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,6 +10,6 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.print("Running");
-	}
+	}	
 
 }
